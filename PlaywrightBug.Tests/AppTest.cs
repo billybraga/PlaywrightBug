be9d7a4554fc1dddad64e1121571f31c919434a0
Billy Braga <billy.braga@gmail.com>
2023-05-11 10:20:32 -0400

    first commit

---
+++ PlaywrightBug.Tests/AppTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace PlaywrightBug.Tests;
+
+[TestFixture]
+public class AppTest : PageTest
+{
+    [Test]
+    public async Task Test()
+    {
+        await Page.GotoAsync("https://google.com");
+    }
+}
